572761c94e5d8f445c8dd086b75d7eb8e0e56e7a
kokopelli314 <nacclonts@gmail.com>
2018-09-06 22:28:43 -0600

    implement hamburger menu for mobile

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -7,9 +7,7 @@ var vm = new Vue({
 		contactPhone: '',
 		contactMessage: ''
 	},
-	mounted: function() {
-		console.log(`Vue mounted`);
-	},
+	mounted: function() { },
 	methods: {
 		isActive: function(text, elementId) {
 			return (text !== '');
@@ -17,6 +15,24 @@ var vm = new Vue({
 		checkPasscode(event) {
 			if (event.target.value === '2727') {
 				this.showPageBlocker = false;
+			}
+		},
+		openMenu(event) {
+			this.toggleMenu(true, event)
+		},
+		closeMenu(event) {
+			this.toggleMenu(false, event)
+		},
+		toggleMenu(showMenu, event) {
+			event.preventDefault()
+			let menu = this.$refs.mainMenu
+
+			if (showMenu) {
+				menu.setAttribute('aria-hidden', false)
+				menu.setAttribute('aria-expanded', true)
+			} else {
+				menu.setAttribute('aria-hidden', true)
+				menu.setAttribute('aria-expanded', false)
 			}
 		}
 	}
