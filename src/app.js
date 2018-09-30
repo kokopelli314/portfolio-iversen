8d05d2ffb10147bba37f801a6a0f13b2228488b5
kokopelli314 <nacclonts@gmail.com>
2018-09-30 12:22:15 -0600

    add comments

--- src/app.js
+++ src/app.js
@@ -4,17 +4,22 @@ const path = require('path')
 const app = express()
 const router = express.Router()
 
+///////////////////
+// Configuration
 const PORT = process.env.PORT
-const ROOT_PATH = process.env.ROOT_PATH || '/'
 const STATIC_DIR = path.join(__dirname, 'public')
+// While developing, set ROOT_PATH to the URL prefix that'll be used in production.
+// This helps make sure absolute/relative paths come out right.
+const ROOT_PATH = process.env.ROOT_PATH || '/'
 
 // Serve static files and image media
 app.use(ROOT_PATH, express.static(STATIC_DIR))
 app.use(ROOT_PATH, express.static(process.env.MEDIA_DIR))
-
-// Pug
+// Template engine === Pug
 app.set('view engine', 'pug')
 
+///////////////////
+// Views
 router.get('/', (req, res) => {
 	res.render(path.join(`${STATIC_DIR}`, 'index'))
 })
@@ -27,9 +32,9 @@ router.get('/work/ux', (req, res) => {
 	res.render(path.join(`${STATIC_DIR}`, 'work-ux'))
 })
 
-
 app.use(ROOT_PATH, router)
 
+///////////////////
 // Start it up
 app.listen(PORT, () => {
 	console.log(`Express is listening on port ${PORT}!`)
